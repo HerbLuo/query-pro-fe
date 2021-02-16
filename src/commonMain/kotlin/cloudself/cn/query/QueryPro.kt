d68963a92129449f427add47c073b6fb0882ae85
HerbLuo <cloudself.cn@gmail.com>
2021-02-16 18:04:20 +0800

    修复left join中的一个bug

--- src/commonMain/kotlin/cloudself/cn/query/QueryPro.kt
+++ src/commonMain/kotlin/cloudself/cn/query/QueryPro.kt
@@ -50,25 +50,48 @@ abstract class QueryProImpl<
         fields2: IFieldGenerator
     ): QueryPro<QUERY, WHERE_FIELD, ORDER_BY_FIELD, FIELD_GENERATOR> {
         val oldFrom = queryStructure.from
+        val oldJoins = oldFrom.joins
         val currentTableName = queryStructure.from.main
-        val foreignTableName: String
-        val foreignFields: List<Field>
-        val currentTableFields: List<Field>
-        if (fields1._getTableName() == currentTableName) {
-            foreignTableName = fields2._getTableName()
+        val field1TableName = fields1._getTableName()
+        val field2TableName = fields2._getTableName()
+
+        var foreignTableName: String? = null
+        var foreignFields: List<Field>? = null
+        var currentTableFields: List<Field>? = null
+
+        if (field1TableName == currentTableName) { // 其中一个是主表
+            foreignTableName = field2TableName
             foreignFields = fields2._getFields()
             currentTableFields = fields1._getFields()
-        } else {
-            foreignTableName = fields1._getTableName()
+        } else if (field2TableName == currentTableName) { // 其中一个是主表
+            foreignTableName = field1TableName
             foreignFields = fields1._getFields()
             currentTableFields = fields2._getFields()
+        } else { // 没有主表，从joins里面找已有表
+            for (oldJoin in oldJoins) {
+                if (oldJoin.table == field1TableName) {
+                    foreignTableName = field2TableName
+                    foreignFields = fields2._getFields()
+                    currentTableFields = fields1._getFields()
+                    break
+                }
+                if (oldJoin.table == field2TableName) {
+                    foreignTableName = field1TableName
+                    foreignFields = fields1._getFields()
+                    currentTableFields = fields2._getFields()
+                    break
+                }
+            }
+        }
+        if (foreignFields == null || currentTableFields == null || foreignTableName == null) {
+            throw RuntimeException("can not find left table for joiner1 and joiner2: $field1TableName, $field2TableName")
         }
         if (foreignFields.size != currentTableFields.size) {
             throw RuntimeException("the joiner length of $currentTableFields and $foreignFields is not equal")
         }
 
         val foreignJoinerOn = foreignFields.mapIndexed { index, field -> FromJoinerOn(currentTableFields[index], field) }
-        val newJoins = oldFrom.joins + FromJoiner(foreignTableName, foreignJoinerOn.toTypedArray())
+        val newJoins = oldJoins + FromJoiner(foreignTableName, foreignJoinerOn.toTypedArray())
 
         return createQuery(queryStructure.copy(from = oldFrom.copy(joins = newJoins)))
     }
